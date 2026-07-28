11a106153ca0c1d470da3e5b845c9ad09bf9b04b
Brian <edwardbrian96@gmail.com>
2026-07-28 08:24:56 -0500

    feat: added load build production

--- build-production.sh
+++ build-production.sh
@@ -4,6 +4,7 @@ echo "🔧 Construyendo imagen Docker para production..."
 
 docker build \
   --platform linux/amd64 \
+  --load \
   -t edrians/whatsapp-ecommerce:v1 .
 
-echo "✅ Imagen creada: edrians/whatsapp-ecommerce:v1 ."
+echo "✅ Imagen creada y cargada en Docker: edrians/whatsapp-ecommerce:v1"
